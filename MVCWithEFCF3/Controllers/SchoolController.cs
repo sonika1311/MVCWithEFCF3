d12fd19fa03a4c32bbbff017fefcf4d74f110f74
sonika1311 <sonikakumaribom@gmail.com>
2024-08-09 01:36:35 +0530

    run the project and it created databse and added record and dispalyed on webpage

---
+++ MVCWithEFCF3/Controllers/SchoolController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWithEFCF3.Models;
+
+namespace MVCWithEFCF3.Controllers
+{
+    public class SchoolController : Controller
+    {
+        SchoolDBContext dc = new SchoolDBContext();
+        public ActionResult Index()
+        {
+            Student s = new Student { Name = "Raju" };
+            dc.Students.Add(s);
+            dc.SaveChanges();
+            return View(dc.Students);
+        }
+    }
+}
--- MVCWithEFCF3/Models/SchoolDBContext.cs
+++ MVCWithEFCF3/Models/SchoolDBContext.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Data.Entity;
 
 namespace MVCWithEFCF3.Models
 {
-    public class SchoolDBContext 
+    public class SchoolDBContext : DbContext
     {
+        public SchoolDBContext() : base("ConStr")
+        {
+
+        }
+        public DbSet<Student> Students
+        {
+            get; set;
+        }
     }
 }
